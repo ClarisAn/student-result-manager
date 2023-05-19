7c437732f70c16227f4216e6e4ebfd16663cd7e6
clarissacoetzee <clarissa@makeithappen.africa>
2023-05-18 21:17:09 -0400

    [CC] Put in more Unit Tests

--- main/test/UnitTests.py
+++ main/test/UnitTests.py
@@ -5,10 +5,14 @@ import os
 import unittest
 from main.src.models.studentManagement import StudentManager
 from main.src.database.database import Database
+from main.src.models.courseManagement import CourseManager
+from main.src.models.resultManagement import ResultsManager
 
 # Declare class in order to test functions in class
 
 student_manager = StudentManager()
+course_manager = CourseManager()
+result_manager = ResultsManager()
 
 
 class Test(unittest.TestCase):
@@ -34,8 +38,8 @@ class Test(unittest.TestCase):
                             database=os.environ['dbName'],
                             reconnect="False")
 
-        result = database.execute('SELECT * FROM students')
-        self.assertIsNotNone(result)
+        result = database.execute("SELECT * FROM results WHERE id = '';")
+        self.assertIsNone(result)
 
     # Unit Tests for Student Management Class
     def test_student_age(self):
@@ -52,6 +56,30 @@ class Test(unittest.TestCase):
 
     def test_all_students(self):
         result = student_manager.get_all_student_info()
+        self.assertIsNotNone(result)
+
+    # Unit Tests for Course Management Class
+    def test_all_courses(self):
+        result = course_manager.get_all_courses()
+        self.assertIsNotNone(result)
+
+    def test_course_add(self):
+        result = course_manager.create_course_info("TEST01", "Testing")
+        status = result['status']
+        self.assertEqual(status, 'SUCCESS')
+
+    def test_course_delete(self):
+        result = course_manager.delete_course("TEST01")
+        status = result['status']
+        self.assertEqual(status, 'SUCCESS')
+
+    def test_code(self):
+        result = course_manager.check_code("TEST01")
+        self.assertEqual(result, True)
+
+    # Unit Tests for Course Management Class
+    def test_all_results(self):
+        result = result_manager.get_all_results()
         self.assertIsNotNone(result)
 
 
